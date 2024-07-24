7cffa47113e2f4c086b1d5ac413db2a6696cd0d6
iFallenHunt <lukas_santos.silva@hotmail.com>
2024-07-24 15:29:53 -0300

    First Commit

---
+++ src/test/java/com/tech/postal_code/PostalCodeRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.postal_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalCodeRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
